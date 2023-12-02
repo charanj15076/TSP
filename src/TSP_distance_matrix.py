9bac5c176208d3a8945a026f99b0cd1088aac411
Anirudh <anirudh.venkatesh@csu.fullerton.edu>
2023-12-01 21:41:11 -0800

    bbox

--- src/TSP_distance_matrix.py
+++ src/TSP_distance_matrix.py
@@ -18,7 +18,11 @@ def solve_tsp(location,technique):
         mapper_dict[city]=i
         city+=1
     """
-    
+    locations=list(location.values())
+    latitudes=[loc[1] for loc in locations]
+    longitudes=[loc[0] for loc in locations]
+    #print(latitudes)
+    #print(longitudes)
     lookup=dict()
     # Create a complete graph from the distance matrix
     """
@@ -30,7 +34,17 @@ def solve_tsp(location,technique):
             dist = dist
             G1.add_edge(i, j, weight=dist)
     """
-    G1 = ox.graph_from_point((33.72773047528036, -117.95608520507814), dist=80, network_type='walk')
+    
+    north = max(latitudes)
+    south = min(latitudes) 
+    west = min(longitudes)
+    east = max(longitudes)
+    print(north)
+    
+    # Download the street network graph for the specified area
+    G1 = ox.graph_from_bbox(north, south, east, west,retain_all=True, truncate_by_edge=True,network_type='drive')
+    #G1 = ox.graph_from_point((33.72773047528036, -117.95608520507814), dist=80, network_type='walk')
+    print(G1)
     G = nx.Graph()
     cordinates=list(location.values())
     for i in range(len(cordinates)):
@@ -69,21 +83,28 @@ def solve_tsp(location,technique):
     else:
         tsp_solution = christofides(G,weight="weight")
     return tsp_solution,lookup
+
     
 # Example locations with city names as keys and (latitude, longitude) as values
+#latitude, longtiude
 locations = {
     "City1": [33.725303380272706, -117.96016216278078],
     "City2": [33.72291191070952, -117.95501232147217],
     "City3": [33.728658464055954, -117.96136379241945],
     "City4": [33.72773047528036,  -117.95608520507814],
 }
+#locations=list(locations1.values())
+#print(locations)
 
-tsp_solution, lookup = solve_tsp(locations,"Christofides")
-print(tsp_solution)
-print(lookup)
-tsp_solution, lookup = solve_tsp(locations,"Simulated Annealing")
-print(tsp_solution)
-print(lookup)
+#tsp_solution, lookup = solve_tsp(locations,"Christofides")
+#print(tsp_solution)
+#print(lookup)
+#tsp_solution, lookup = solve_tsp(locations,"Simulated Annealing")
+#print(tsp_solution)
+#print(lookup)
 tsp_solution, lookup = solve_tsp(locations,"Greedy")
+#solve_tsp(locations,"Greedy")
 print(tsp_solution)
 print(lookup)
+print("Hello")
+
