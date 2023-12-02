c2c21bebae7ef894d581bcb57399f2a5de8eab54
Anirudh <anirudh.venkatesh@csu.fullerton.edu>
2023-12-01 17:09:09 -0800

    Added source and destination nodes and their distance

--- src/TSP_distance_matrix.py
+++ src/TSP_distance_matrix.py
@@ -1,12 +1,14 @@
 from geopy.distance import geodesic
 import math
 import networkx as nx
+import osmnx as ox
 from approximation_techniques.christofides import christofides
 from approximation_techniques.simulated_annealing import simulated_annealing
 from approximation_techniques.greedy import greedy
 #from networkx.algorithms.approximation.traveling_salesman import simulated_annealing_tsp
 
 def solve_tsp(location,technique):
+    """
     #Convert location dictionary to pos dictionary
     city=0
     pos=dict()
@@ -15,37 +17,69 @@ def solve_tsp(location,technique):
         pos[city]=location[i]
         mapper_dict[city]=i
         city+=1
+    """
     
+    lookup=dict()
     # Create a complete graph from the distance matrix
-    G = nx.Graph()
+    """
+    G1 = nx.Graph()
     #pos[0] = (0.5, 0.5)
     for i in range(len(pos)):
         for j in range(i + 1, len(pos)):
             dist = math.hypot(pos[i][0] - pos[j][0], pos[i][1] - pos[j][1])
             dist = dist
-            G.add_edge(i, j, weight=dist)
+            G1.add_edge(i, j, weight=dist)
+    """
+    G1 = ox.graph_from_point((33.72773047528036, -117.95608520507814), dist=80, network_type='walk')
+    G = nx.Graph()
+    cordinates=list(location.values())
+    for i in range(len(cordinates)):
+        for j in range(len(cordinates)):
+            if i==j:
+                continue
+            source=ox.distance.nearest_nodes(G1,cordinates[i][1],cordinates[i][0], return_dist=False)
+            destination=ox.distance.nearest_nodes(G1,cordinates[j][1],cordinates[j][0], return_dist=False)
+            shortest_path_length=nx.shortest_path_length(G1,source,destination)
+            shortest_paths=nx.shortest_path(G1,source,destination)
+            G.add_edge(source, destination, weight=shortest_path_length,)
+            lookup[(source,destination)]=shortest_paths
 
+    """
+    G1 = ox.graph_from_point((33.72773047528036, -117.95608520507814), dist=80, network_type='walk')
+    source=ox.distance.nearest_nodes(G1,-117.96016216278078,33.725303380272706, return_dist=False)
+    destination=ox.distance.nearest_nodes(G1,-117.95501232147217,33.72291191070952, return_dist=False)
+    #print(source)
+    shortest_path_length=nx.shortest_path_length(G1,source,destination)
+    print(type(shortest_path_length))
+    shortest_paths=nx.shortest_path(G1,source,destination)
+    print(shortest_paths)
+    #path=dict(nx.all_pairs_shortest_path(G1))
+    #print(G1)
+    #print(path)
+    #G=nx.Graph(G1)
+    """
+    
     if (technique=="Christofides"):
-        tsp_solution = christofides(G,pos,mapper_dict,weight="weight")
+        tsp_solution = christofides(G,weight="weight")
     elif (technique=="Simulated Annealing"):
-        tsp_solution = simulated_annealing(G,pos,mapper_dict,weight="weight",epochs=50,temperature=300)
+        tsp_solution = simulated_annealing(G,weight="weight",epochs=50,temperature=300)
     elif (technique=="Greedy"):
-        tsp_solution = greedy(G,pos,mapper_dict,weight="weight")
+        tsp_solution = greedy(G,weight="weight")
     #Default
     else:
-        tsp_solution = christofides(G,pos,mapper_dict,weight="weight")
-    return tsp_solution
-
+        tsp_solution = christofides(G,weight="weight")
+    return tsp_solution,lookup
+    
 # Example locations with city names as keys and (latitude, longitude) as values
 locations = {
-    "City1": [52.5200, 13.4050],
-    "City2": [53.5511, 9.9937],
-    "City3": [48.1351, 11.5820],
-    "City4": [50.9375, 6.9603],
-    "City5": [50.1109, 8.6821]
+    "City1": [33.725303380272706, -117.96016216278078],
+    "City2": [33.72291191070952, -117.95501232147217],
+    "City3": [33.728658464055954, -117.96136379241945],
+    "City4": [33.72773047528036,  -117.95608520507814],
 }
 
-#tsp_solution = solve_tsp(locations,"Christofides")
-tsp_solution = solve_tsp(locations,"Simulated Annealing")
-#tsp_solution = solve_tsp(locations,"Greedy")
+tsp_solution, lookup = solve_tsp(locations,"Christofides")
+#tsp_solution, lookup = solve_tsp(locations,"Simulated Annealing")
+#tsp_solution, lookup = solve_tsp(locations,"Greedy")
 print(tsp_solution)
+print(lookup)
--- src/api/services.py
+++ src/api/services.py
@@ -8,17 +8,17 @@ import osmnx
 # Input: center coordinates, list of blockage coordinates, distance
 # Output: shortest distance & path matrices
 def run_simulation(blockages_list, lat_center, lng_center, distance):
-
-	G = generate_graph_from_centroid(lat_center, lng_center, distance)
+        
+        G = generate_graph_from_centroid(lat_center, lng_center, distance)
 
 	# build all-pairs shortest paths
 	new_G = all_pairs_shortest_paths(G, locations)
 
 	if (technique=="Christofides"):
-        tsp_solution = christofides(new_G, mapper_dict,weight="weight")
-    elif (technique=="Simulated Annealing"):
-        tsp_solution = simulated_annealing(new_G,mapper_dict,weight="weight",epochs=50,temperature=300)
-    elif (technique=="Greedy"):
+                tsp_solution = christofides(new_G, mapper_dict,weight="weight")
+        elif (technique=="Simulated Annealing"):
+                tsp_solution = simulated_annealing(new_G,mapper_dict,weight="weight",epochs=50,temperature=300)
+        elif (technique=="Greedy"):
         tsp_solution = greedy(new_G,mapper_dict,weight="weight")
     #Default
     else:
